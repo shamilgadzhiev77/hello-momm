780d47118891b3f075c572f36a037f089f66ae5b
shamilgadzhiev77 <146872052+shamilgadzhiev77@users.noreply.github.com>
2023-10-03 20:45:49 +0300

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+// const button = document.querySelector("button");
+// const output = document.querySelector(".output");
+
+// button.addEventListener("click", function () {
+//   output.innerHTML = "Мама, я люблю тебя!";
+// });
+
+const button = document.querySelector("button");
+const output = document.querySelector(".output");
+
+button.addEventListener("click", function () {
+  output.innerHTML = "Мама , я тебя люблю!!!";
+});
