4d43ba5bc4b24be841d9244a7b563aeeb8464452
shamilgadzhiev77 <146872052+shamilgadzhiev77@users.noreply.github.com>
2023-10-03 21:15:42 +0300

    index.js

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ const button = document.querySelector("button");
 const output = document.querySelector(".output");
 
 button.addEventListener("click", function () {
-  output.innerHTML = "Мама , я тебя люблю!!!";
+  output.innerHTML = "Мама! Прости меня, за то что я не могу отдать тебе должное, Мама!!!";
 });
